37ddc6a4747c46c89dfe1a4986f8588e78baece6
Dan Wright <daniel-g-wright@hotmail.co.uk>
2020-09-21 17:28:58 +0000

    Fix calculator and emailer values using a switch statement so that the colour name, amount and price successfully pull through into the email sent to the user.

--- assets/js/emailer.js
+++ assets/js/emailer.js
@@ -1,7 +1,11 @@
 function sendMail(contactForm) {
     emailjs.send("service_26ufq5f", "template_w42ccsw", {
         "first_name": contactForm.fname.value,
-        "emailaddress": contactForm.emailaddress.value
+        "emailaddress": contactForm.emailaddress.value,
+        "paintcolour": contactForm.choose_colour_two.value,
+        "quantity": contactForm.square_feet.value,
+        "price": contactForm.price.value
+
     })
     .then(
         function(response) {
--- assets/js/pricecalculator.js
+++ assets/js/pricecalculator.js
@@ -1,12 +1,45 @@
 var basePrice = 25;
 
+/* Added the getColour function as I was previously using the values to calculate the price. This meant however that the values couldnt describe the colour for the email. A switch statement allowed me to keep the colour values and still do the appropriate calulations */
+function getColour() {
+  var colour = $("#dropdown-calculator").val();
+  switch (colour) {
+    case "Blaze Red": colourIndex = 1.0;
+    break;
+    case "Azure Blue": colourIndex = 1.0;
+    break;
+    case "Jewel Green": colourIndex = 1.1;
+    break;
+    case "Passion Purple": colourIndex = 1.3;
+    break;
+    case "Vivid Pink": colourIndex = 1.3;
+    break;
+    case "Sunburnt Orange": colourIndex = 1.2;
+    break;
+    case "Calming Teal": colourIndex = 1.25;
+    break;
+    default: colourIndex = "Error";
+  }
+  return colourIndex;
+  }
+
 function calculatePrice() {
-  /*var price = */
+  getColour();
+  var sqFeet = $("#square-feet").val();
+  var price = "£" + (basePrice + (sqFeet * colourIndex));
+  $("#priceBox").val(price);
+}
+
+
+/*
+function calculatePrice() {
+  /*var price = 
   var colour = $("#dropdown-calculator").val();
   var sqFeet = $("#square-feet").val();
   var price = "£" + (basePrice + (sqFeet * colour));
   $("#priceBox").val(price);
 }
+*/
 
 /* was using .change however the selection was not dynamically updating - attempting to use .on to remedy:
 https://stackoverflow.com/questions/13418963/jquery-onchange-function-not-triggering-for-dynamically-created-inputs */
