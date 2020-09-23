92553ff68a22ffb8ed68c0aa002947213906b59d
Dan Wright <daniel-g-wright@hotmail.co.uk>
2020-09-23 14:55:18 +0000

    Remove excess images and amend font choice and colours across the site.

--- assets/js/scroller.js
+++ assets/js/scroller.js
@@ -2,16 +2,18 @@ $(window).scroll(function() {
 if ($(".navbar").offset().top > 50) {
     $('#brand').text('P.');
     $(".navbar").removeClass("navbar-expand-lg");
+    $(".navbar").addClass("translucent-nav");
 } else {
     $('#brand').text('PaintPeruser');
     $(".navbar").addClass("navbar-expand-lg");
+    $(".navbar").removeClass("translucent-nav");
 }   
 });
 
 var icon = document.getElementsByClassName("navbar-toggler-icon");
 var pushDown = document.getElementById("push");
 
-$(document).ready(function(){
+/*$(document).ready(function(){
   $(icon[0]).click(function(){
     if($(pushDown).hasClass("push")){
       pushDown.className = "pushUp";
@@ -20,4 +22,4 @@ $(document).ready(function(){
       pushDown.className = "push";
     }
   })
-});
+});*/
