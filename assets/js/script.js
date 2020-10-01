19ecaa479c1d0d30b4be42b409c4686b2cbf90c1
Dan Wright <daniel-g-wright@hotmail.co.uk>
2020-10-01 20:35:15 +0000

    Add 'pick me' button to index.html to pull current colour choice down into the quote box, and update the price accordingly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,29 +19,6 @@
 });
 */
 
-$(document).ready(function() {
-    $("#blaze-red").click(function(){
-        $("#main-image").removeClass("passion-purple jewel-green azure-blue calming-teal sunburnt-orange vivid-pink default-hue").addClass("blaze-red");
-    });
-    $("#azure-blue").click(function(){
-        $("#main-image").removeClass("passion-purple jewel-green blaze-red calming-teal sunburnt-orange vivid-pink default-hue").addClass("azure-blue");
-    });
-    $("#jewel-green").click(function(){
-        $("#main-image").removeClass("passion-purple blaze-red azure-blue calming-teal sunburnt-orange vivid-pink default-hue").addClass("jewel-green");
-    });
-    $("#passion-purple").click(function(){
-        $("#main-image").removeClass("blaze-red jewel-green azure-blue calming-teal sunburnt-orange vivid-pink default-hue").addClass("passion-purple");
-    });
-    $("#vivid-pink").click(function(){
-        $("#main-image").removeClass("passion-purple jewel-green azure-blue calming-teal sunburnt-orange blaze-red default-hue").addClass("vivid-pink");
-    });
-    $("#calming-teal").click(function(){
-        $("#main-image").removeClass("passion-purple jewel-green azure-blue blaze-red sunburnt-orange vivid-pink default-hue").addClass("calming-teal");
-    });
-    $("#sunburnt-orange").click(function(){
-        $("#main-image").removeClass("passion-purple jewel-green azure-blue calming-teal blaze-red vivid-pink default-hue").addClass("sunburnt-orange");
-    });
-});
 
 $(document).ready(function() {
     $("#blaze-red-btn").click(function(){
@@ -66,6 +43,32 @@ $(document).ready(function() {
         $("#main-image").removeClass("passion-purple jewel-green azure-blue calming-teal blaze-red vivid-pink default-hue").addClass("sunburnt-orange");
     });
 });
+
+
+$('#pick-me-btn').on('click', function() {
+  var current_choice = $("#main-image").attr("class");
+  switch (current_choice) {
+  case "blaze-red": current_choice = "Blaze Red";
+  break;
+  case "azure-blue": current_choice = "Azure Blue";
+  break;
+  case "jewel-green": current_choice = "Jewel Green";
+  break;
+  case "passion-purple": current_choice = "Passion Purple";
+  break;
+  case "vivid-pink": current_choice = "Vivid Pink";
+  break;
+  case "sunburnt-orange": current_choice = "Sunburnt Orange";
+  break;
+  case "calming-teal": current_choice = "Calming Teal";
+  break;
+  default: current_choice = "Error";
+  };
+  $('#dropdown-calculator').val(current_choice);
+  console.log(current_choice);
+  calculatePrice();
+});
+
 
 
 /* Experiment with if statements to remove the classes
